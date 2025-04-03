8a4ad27a2c866d97e20c7795be54a700cd99c9f1
Zhang-WuWa <zzq_mc@163.com>
2025-04-03 18:37:04 +0800

    use config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = [
+    'configs/carlotta_20.js',
+    'configs/chun_20.js',
+    'configs/chun_1097k.js',
+    'configs/jiyan_6336k.js',
+    'configs/jiyan_20.js',
+    'configs/phobe_822k.js',
+    'configs/phobe_20.js',
+    'configs/zhezhi_20.js',
+]
