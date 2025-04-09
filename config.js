bb9d4454a1ea9241d7e8adca8832a96911ef7106
Zhang-WuWa <zzq_mc@163.com>
2025-04-09 23:31:10 +0800

    phobe add /check

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ const config = [
     'configs/chun_20.js',
     'configs/jiyan_6336k.js',
     'configs/jiyan_20.js',
-    'configs/phobe_822k.js',
     'configs/phobe_20.js',
     'configs/zhezhi_20.js',
     'configs/encore.js',
