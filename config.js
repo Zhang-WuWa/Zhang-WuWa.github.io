ce52197fe4522203d0dd2cc7180bc41b68943ad1
Zhang-WuWa <zzq_mc@163.com>
2025-04-10 00:04:55 +0800

    jiyan add /check

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const config = [
     'configs/carlotta_20.js',
     'configs/chun_20.js',
-    'configs/jiyan_6336k.js',
     'configs/jiyan_20.js',
     'configs/phobe_20.js',
     'configs/zhezhi_20.js',
