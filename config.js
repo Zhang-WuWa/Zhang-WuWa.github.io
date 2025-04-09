b94064175c32c71fe451b8e9c3adee01ed9ee225
Zhang-WuWa <zzq_mc@163.com>
2025-04-09 23:21:46 +0800

    chun add /check

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const config = [
     'configs/carlotta_20.js',
     'configs/chun_20.js',
-    'configs/chun_1097k.js',
     'configs/jiyan_6336k.js',
     'configs/jiyan_20.js',
     'configs/phobe_822k.js',
