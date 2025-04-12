64a86c863a8d954312025e03e110f0c779bba3f9
Zhang-WuWa <zzq_mc@163.com>
2025-04-12 22:07:33 +0800

    remove config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = [
-    'configs/carlotta_20.js',
-    'configs/chun_20.js',
-    'configs/jiyan_20.js',
-    'configs/phobe_20.js',
-    'configs/zhezhi_20.js',
-    'configs/encore.js',
-    'configs/jinxi.js',
-]
